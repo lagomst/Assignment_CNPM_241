fe37a9b2422df2cc45474a0e40c454a729ab5ddb
hungminhlgm <167613516+hungminhlgm@users.noreply.github.com>
2024-11-27 21:55:15 +0700

    Delete backend directory

--- backend/routes/admin.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const adminController = require("../controllers/admin.controller");
-const router = express.Router();
-
-router.post('/add-printer', adminController.addPrinter);
-
-module.exports = router;
